3fe6cb8a476a1b69fe5f51601d2aefa414175bc9
Ivan Kukushkin <github-sync@outlook.com>
2023-02-19 19:33:58 +0400

    fix: технический коммит, потребовался для отработки импорта в espruino

--- js/module/ModuleAppError.js
+++ js/module/ModuleAppError.js
@@ -1,16 +1,11 @@
-/**
- * @class
- * Класс ClassAppError наследует и расширяет возможности базового класса ошибок.
- */
 class ClassAppError {
     constructor(_message, _code) {
-        //super(_message);
-        this.name = 'ClassAppError'; //переопределяем имя типа
+        this.name = 'ClassAppError'; 
         this._Message = _message;
-        this._Code = _code || 1; //по умолчанию присвоить код неизвестной ошибки
+        this._Code = _code || 1; 
     }
     get Message() {return this._Message;}
     get Code() {return this._Code;}
 }
 
-exports = ClassAppError; //экспортируем класс, ВНИМАНИЕ - именно класс а не объект!
+exports = ClassAppError;
