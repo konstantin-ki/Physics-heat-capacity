157ab6d8cc21295608fcca31848fc453172678d9
Ivan Kukushkin <github-sync@outlook.com>
2023-02-21 16:41:53 +0400

    refactor: When working out how the remote connection of modules works in Espruino (via github), we had to split one module in which all three classes necessary for the operation of the piezo-emitter module were exported into 3 separate files in each of which only one class is exported. При отработки того как в Espruino работает удаленное (через github) подключение модулей пришлось разбить один модуль в котором был экспорт всех трех классов необходимых для работы модуля пьезоизлучателя, на 3 отдельных файла в каждом из которых, производится экспорт только одного класса.

---
+++ js/module/ModuleBuzzerType.js
@@ -0,0 +1,57 @@
+//экспортировать объект прикладных ошибок
+const err = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleAppError.js');
+
+/**
+ * @class
+ * Класс ClassBuzzerType обеспечивает прикладной тип для конструктора класса ClassBuzzer.
+ * Пример набора аргументов конструктора:
+ * {A4 /порт/, 4500 /герц/}
+ */
+class ClassBuzzerType {
+    /**
+     * @constructor
+     * @param {Object} _buzPin      - Pin на котором сидит buzzer
+     */
+    constructor(_buzPin) {
+        this.name = 'ClassTypeBuzzer'; //переопределяем имя типа
+        this._BuzPin = undefined; 
+
+        this.Init(_buzPin); //инициализировать поля
+    }
+    /*******************************************CONST********************************************/
+    /**
+     * @const
+     * @type {number}
+     * Константа ERROR_CODE_ARG_VALUE определяет код ошибки, которая может произойти
+     * в случае передачи не валидных данных
+     */
+    static get ERROR_CODE_ARG_VALUE() { return 10; }
+    /**
+     * @const
+     * @type {string}
+     * Константа ERROR_MSG_ARG_VALUE определяет сообщение ошибки, которая может произойти
+     * в случае передачи не валидных данных
+     */
+    static get ERROR_MSG_ARG_VALUE() { return `ERROR>> invalid data. ClassID: ${this.name}`; }
+    /*******************************************END CONST****************************************/
+    /**
+     * Метод инициализирует поля объекта
+     */
+    Init(_buzPin) {        
+        /*проверить переданные аргументы на валидность*/
+        if ((typeof (_buzPin) === 'undefined')) {
+            
+            throw new err(ClassTypeBuzzer.ERROR_MSG_ARG_VALUE ,
+                          ClassTypeBuzzer.ERROR_CODE_ARG_VALUE);
+        }
+        if(!(_buzPin instanceof Pin)) {
+            
+            throw new err(ClassTypeBuzzer.ERROR_MSG_ARG_VALUE,
+                          ClassTypeBuzzer.ERROR_CODE_ARG_VALUE);
+        }
+        /*инициализировать поля*/
+        this._BuzPin = _buzPin;
+    }
+}
+
+exports = ClassBuzzerType;
---
+++ js/module/ModuleBuzzerTypePlay.js
@@ -0,0 +1,75 @@
+//экспортировать объект прикладных ошибок
+const err = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleAppError.js');
+
+/**
+ * @class
+ * Класс ClassBuzzerTypePlay обеспечивает прикладной тип для методов класса ClassBuzzer.
+ * Пример набора аргументов конструктора:
+ * {4 /импульса/, 200 /ms длительность/, 0.5 /50% скважность/}
+ */
+class ClassBuzzerTypePlay {
+    /**
+     * @constructor
+     * @param {number} _pulseDur    - длительность звучания в ms [50<=x<infinity]
+     * @param {number} _numRep      - количество повторений [1...n]
+     * @param {number} _freq        - рабочая частота buzzer
+     * @param {number} _prop        - пропорция ЗВУК/ТИШИНА на одном периоде [0<x<=1]
+     */
+    constructor(_pulseDur, _numRep, _freq, _prop) {
+        this.name = 'ClassTypeBuzzerPlay'; //переопределяем имя типа
+        this._PulseDur = undefined;
+        this._NumRep = undefined;
+        this._Freq = undefined;
+        this._Proportions = undefined;
+        
+        this.Init(_pulseDur, _numRep, _freq, _prop); //инициализировать поля
+    }
+    /*******************************************CONST********************************************/
+    /**
+     * @const
+     * @type {number}
+     * Константа ERROR_CODE_ARG_VALUE определяет код ошибки, которая может произойти
+     * в случае передачи в конструктор не валидных данных
+     */
+    static get ERROR_CODE_ARG_VALUE() { return 10; }
+    /**
+     * @const
+     * @type {string}
+     * Константа ERROR_MSG_ARG_VALUE определяет сообщение ошибки, которая может произойти
+     * в случае передачи в конструктор не валидных данных
+     */
+    static get ERROR_MSG_ARG_VALUE() { return `ERROR>> invalid data. ClassID: ${this.name}`; }
+    /*******************************************END CONST****************************************/
+    /**
+     * Метод инициализирует поля объекта
+     */
+    Init(_pulseDur, _numRep, _freq, _prop) {
+        let pulsedur = _pulseDur || 100;
+        let numrep = _numRep || 1;
+        let freq = _freq || 4000;
+        let prop = _prop || 0.5;
+        
+        /*проверить переданные аргументы  на валидность*/
+        if (!(typeof (pulsedur) === 'number')   ||
+            !(typeof (numrep) === 'number')     ||
+            !(typeof (freq) === 'number')       ||
+            !(typeof (prop) === 'number')       ||
+            !(Number.isInteger(pulsedur))       ||
+            !(Number.isInteger(numrep))         ||
+            !(Number.isInteger(freq))) {
+                
+                throw new err(ClassTypeBuzzerPlay.ERROR_MSG_ARG_VALUE,
+                              ClassTypeBuzzerPlay.ERROR_CODE_ARG_VALUE);
+        }
+        if (pulsedur<50) {pulsedur=50;} //нормализовать значение длительности импульса звучания
+        if (prop<0 || prop>1) {prop=0.5;}
+        
+        /*инициализировать поля*/
+        this._PulseDur = pulsedur;
+        this._NumRep = numrep;
+        this._Freq = freq;
+        this._Proportions = prop;
+    }
+}
+
+exports = ClassBuzzerTypePlay;
