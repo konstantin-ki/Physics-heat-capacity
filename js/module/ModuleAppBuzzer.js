a292ad25ef3ed2ffc4f17529484423fb9232bed4
Ivan Kukushkin <github-sync@outlook.com>
2023-02-24 20:46:13 +0400

    feat: продолжается поиск решения с модулями подключаемыми с GitHub

--- js/module/ModuleAppBuzzer.js
+++ js/module/ModuleAppBuzzer.js
@@ -5,5 +5,8 @@ exports.connect = function(_opt) {
     const ClassBuzzerTypePlay   = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzerTypePlay.min.js');
     const ClassBuzzer           = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzer.min.js');
 
-    return new ClassBuzzer(_opt);
+    function create_buzzer(_opt){
+        return new ClassBuzzer(_opt)
+    }
+    return create_buzzer(_opt);
 };
