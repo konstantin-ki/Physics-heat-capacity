589cbb2365684abb94f1aa7f5bd8259d8b593b9f
Ivan Kukushkin <github-sync@outlook.com>
2023-02-24 19:49:20 +0400

    feat: продолжается поиск решения оптимизации работы с модулями подключаемыми с GitHub

--- js/module/ModuleAppBuzzer.js
+++ js/module/ModuleAppBuzzer.js
@@ -1,8 +1,9 @@
-const ClassBuzzerType       = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzerType.min.js');
-const ClassBuzzerTypePlay   = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzerTypePlay.min.js');
-const ClassBuzzer           = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzer.min.js');
 
 
 exports.connect = function(_opt) {
+    const ClassBuzzerType       = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzerType.min.js');
+    const ClassBuzzerTypePlay   = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzerTypePlay.min.js');
+    const ClassBuzzer           = require('https://raw.githubusercontent.com/konstantin-ki/Physics-heat-capacity/ver2/js/module/ModuleBuzzer.min.js');
+
     return new ClassBuzzer(_opt);
 };
